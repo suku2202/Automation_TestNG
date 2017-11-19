61ce5e1a32396fe1e49cfebbb3b2919ffd45a3e1
Sukumar Akula <Yashashwini@PurpleTalk.local>
2017-11-19 16:24:27 +0530

    initial commit

---
+++ src/test/java/com/yashu/automation/testng/FirstTest.java
@@ -0,0 +1,22 @@
+package com.yashu.automation.testng;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+  @Test
+  public void test1() {
+	  System.out.println("test 1");
+  }
+  
+  @Test
+  public void test2() {
+	  System.out.println("test 2");
+  }
+  
+  @Test
+  public void test3() {
+	  System.out.println("test 2");
+  }
+
+
+}
